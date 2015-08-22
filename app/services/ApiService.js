b8edbd7032f79d4a0dafcfc1c592cef429531aa7
Régis Gaidot <rgaidot@gmail.com>
2015-08-22 11:01:23 +0200

    add post on ApiService

--- app/services/ApiService.js
+++ app/services/ApiService.js
@@ -13,6 +13,32 @@ export default class ApiService {
         {
           url: options.url,
           body: options.data,
+          headers: options.headers,
+          json: true,
+          withCredentials: false
+        },
+        (err, response, body) => {
+          if(err){
+            return reject(err);
+          }
+          if(response.statusCode >= 400){
+            return reject(body);
+          }
+          return resolve(body);
+        }
+      );
+    });
+  }
+  post (options = {}) {
+    if (!options.url) {
+      throw new Error('You forgot to specify url.');
+    }
+    return new bluebird( (resolve, reject) => {
+      request.post(
+        {
+          url: options.url,
+          body: options.data,
+          headers: options.headers,
           json: true,
           withCredentials: false
         },
